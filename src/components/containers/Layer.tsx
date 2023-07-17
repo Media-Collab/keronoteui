17e34667528a75921d82d340458cd1d2a8355de6
GregoryInnovo <gregoryinnovo@gmail.com>
2023-07-17 13:01:28 -0500

    [FIX] canvas layers select

--- src/components/containers/Layer.tsx
+++ src/components/containers/Layer.tsx
@@ -1,15 +1,70 @@
+import { useState, useEffect } from "react";
+import Checkbox from "@mui/material/Checkbox";
+
 const Layer = (props: any) => {
-  const { layer, setLayers, layers } = props;
-  const { name, isSelect } = layer;
+  const { layer, setLayers, layers, selectedLayers, setSelectedLayers } = props;
+  const { id, name, isSelect } = layer;
+
+  const [isSelected, setIsSelected] = useState(isSelect);
+
+  useEffect(() => {
+    setIsSelected(selectedLayers.includes(id));
+  }, [selectedLayers, id]);
 
   return (
     <section
       style={{
         padding: "7px",
-        backgroundColor: !isSelect ? "#BFDBFE" : "#83B0E7",
+        backgroundColor: isSelected ? "#83B0E7" : "#BFDBFE",
         borderRadius: "0.5rem",
       }}
     >
+      <section
+        style={{
+          display: "flex",
+          marginBottom: "0.5rem",
+        }}
+      >
+        <Checkbox
+          checked={isSelected}
+          onChange={() => {
+            setIsSelected(!isSelected);
+            if (!isSelected) {
+              setSelectedLayers([...selectedLayers, id]);
+            } else {
+              setSelectedLayers(
+                selectedLayers.filter((selectedId) => selectedId !== id)
+              );
+            }
+          }}
+        />
+
+        <input
+          style={{
+            fontSize: "11px",
+            margin: "0px",
+            background: "white",
+            width: "100%",
+            height: "2rem",
+            textAlign: "center",
+            borderRadius: "0.5rem",
+            fontWeight: "600",
+            border: "none",
+          }}
+          value={name}
+          disabled={!isSelected}
+          onChange={(e) => {
+            setLayers(
+              layers.map((layer: any) => {
+                if (layer.id === id) {
+                  layer.name = e.target.value;
+                }
+                return layer;
+              })
+            );
+          }}
+        />
+      </section>
       <canvas
         style={{
           backgroundColor: "#FFFFFF",
@@ -19,31 +74,6 @@ const Layer = (props: any) => {
           borderRadius: "0.5rem",
         }}
       ></canvas>
-      <input
-        style={{
-          fontSize: "11px",
-          margin: "0px",
-          background: "white",
-          width: "100%",
-          padding: "3px",
-          textAlign: "center",
-          borderRadius: "0.5rem",
-          fontWeight: "600",
-          border: "none",
-        }}
-        value={name}
-        disabled={!isSelect}
-        onChange={(e) => {
-          setLayers(
-            layers.map((layer: any) => {
-              if (layer.isSelect) {
-                layer.name = e.target.value;
-              }
-              return layer;
-            })
-          );
-        }}
-      />
     </section>
   );
 };
