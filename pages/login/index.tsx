9db6206b40b9d773209bb855afc3502c0e88a872
Juan Viveros <jviverosd1@gmail.com>
2023-07-18 23:43:18 -0500

    a

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -6,27 +6,62 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { authProvider } from "src/authProvider";
 
 import { AppIcon } from "src/components/app-icon";
+
+import Image from "next/image";
+
+import bgImg from "./assets/bg.png";
+
+import {
+  useMediaQuery,
+} from "@mui/material";
 export default function Login() {
+  const isMobile = useMediaQuery("(max-width: 600px)");
+
   return (
-    <AuthPage
-      type="login"
-      formProps={{
-        defaultValues: {
-          email: "",
-          password: "",
-        },
+    <div
+      style={{
+        display: isMobile ? "block" : "grid",
+        placeItems: "start",
+        overflow: "hidden",
+        gridTemplateColumns: "1fr 1fr",
       }}
-      title={
-        <ThemedTitleV2
-          collapsed={false}
-          text="Keronote_ui"
-          icon={<AppIcon />}
-        />
-      }
-      
     >
-      <p>Hola Mundo</p>
-      </AuthPage>
+      <div
+        style={{
+          backgroundColor: "white",
+          width: "100%",
+        }}
+      >
+        <AuthPage
+          type="login"
+          formProps={{
+            defaultValues: {
+              email: "",
+              password: "",
+            },
+          }}
+          title={
+            <ThemedTitleV2
+              collapsed={false}
+              text="Keronote_ui"
+              icon={<AppIcon />}
+            />
+          }
+        ></AuthPage>
+      </div>
+      <Image
+        src={bgImg}
+        alt="bg"
+        style={{
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          display: isMobile ? "none" : "block",
+        }}
+      />
+    </div>
   );
 }
 
