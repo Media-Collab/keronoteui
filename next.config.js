e249f0a6bbd752716ec8fd4af9e7634e2493fdb0
GregoryInnovo <gregoryinnovo@gmail.com>
2023-07-19 03:01:22 -0500

    [ADD] kero module

--- next.config.js
+++ next.config.js
@@ -5,4 +5,9 @@ module.exports = {
   images: {
     domains: ["res.cloudinary.com", "cdn.pixabay.com"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
