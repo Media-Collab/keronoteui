692737710aecbf425102bd04b6af2cd0e3da1bc1
GregoryInnovo <gregoryinnovo@gmail.com>
2023-07-19 20:43:47 -0500

    [REMOVE] file

--- next.config.js
+++ next.config.js
@@ -10,4 +10,9 @@ module.exports = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  // output: "export",
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: "dist",
 };
